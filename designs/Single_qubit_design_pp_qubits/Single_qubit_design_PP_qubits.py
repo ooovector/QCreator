e312cd3ddddefb15e1b9f0a19860409af4fe001d
ivan-tsitsilin <74404515+ivan-tsitsilin@users.noreply.github.com>
2021-07-02 09:54:10 +0200

    single qubits pp update

--- designs/Single_qubit_design_pp_qubits/Single_qubit_design_PP_qubits.py
+++ designs/Single_qubit_design_pp_qubits/Single_qubit_design_PP_qubits.py
@@ -37,20 +37,16 @@ layers_configuration = {
     'bandages':19,
 }
 
-sample = creator.Sample('1Q_test',layers_configuration)
+sample = creator.Sample('Single_qubits_PP_2',layers_configuration)
 
 #specify sample vertical and horizontal lengths
-sample.chip_geometry.sample_vertical_size=4.3e3
-sample.chip_geometry.sample_horizontal_size=7e3
-central_line_y = sample.chip_geometry.sample_vertical_size/2
-central_line_x = 1000
-resonator_coupler_length = 300
-
+sample.chip_geometry.sample_vertical_size=6e3
+sample.chip_geometry.sample_horizontal_size=10e3
 
 chip_edge_ground = elements.ChipEdgeGround(sample.chip_geometry, sample.layer_configuration, sample.pads)
 sample.add(chip_edge_ground)
 
-# 1. Create contact pads:
+# 1. Create contact pads for 6*10 pcb WMI from Huns:
 pads_left = []
 pads_right = []
 for pad_side_id in range(1):
@@ -72,27 +68,34 @@ for pad_side_id in range(1):
 
 pads_top = []
 pads_bottom = []
-eth_offset=2470
-for pad_side_id in range(3):
-    pad = elements.Pad('pad-bottom-' + str(pad_side_id),
-                       (sample.chip_geometry.sample_horizontal_size / 2+ eth_offset * (pad_side_id - 1), pad_offset),
-                       -np.pi / 2, tl_core, tl_gap, tl_ground,
-                       layer_configuration=sample.layer_configuration, chip_geometry=sample.chip_geometry,
-                       **elements.default_pad_geometry())
-    pads_bottom.append(pad)
-    sample.add(pad)
-    pad = elements.Pad('pad-top-' + str(pad_side_id),
-                       (sample.chip_geometry.sample_horizontal_size / 2 + eth_offset * (pad_side_id - 1),
-                        sample.chip_geometry.sample_vertical_size - pad_offset),
-                       np.pi / 2, tl_core, tl_gap, tl_ground,
-                       layer_configuration=sample.layer_configuration, chip_geometry=sample.chip_geometry,
-                       **elements.default_pad_geometry())
-    pads_top.append(pad)
-    sample.add(pad)
+huns_offset=2470
+pad_bottom_1 = elements.Pad('pad-bottom-' + str(1),
+                   (sample.chip_geometry.sample_horizontal_size / 2+ huns_offset *(- 1), sample.chip_geometry.sample_vertical_size -pad_offset),
+                   np.pi / 2, tl_core, tl_gap, tl_ground,
+                   layer_configuration=sample.layer_configuration, chip_geometry=sample.chip_geometry,
+                   **elements.default_pad_geometry())
+pad_bottom_2 = elements.Pad('pad-bottom-' + str(2),
+                   (sample.chip_geometry.sample_horizontal_size / 2+ huns_offset, sample.chip_geometry.sample_vertical_size -pad_offset),
+                   np.pi / 2, tl_core, tl_gap, tl_ground,
+                   layer_configuration=sample.layer_configuration, chip_geometry=sample.chip_geometry,
+                   **elements.default_pad_geometry())
+pads_top.append(pad_bottom_1)
+pads_top.append(pad_bottom_2)
+sample.add(pad_bottom_1)
+sample.add(pad_bottom_2)
+pad = elements.Pad('pad-top-' + str(pad_side_id),
+                   (sample.chip_geometry.sample_horizontal_size / 2,
+                     pad_offset),
+                   -np.pi / 2, tl_core, tl_gap, tl_ground,
+                   layer_configuration=sample.layer_configuration, chip_geometry=sample.chip_geometry,
+                   **elements.default_pad_geometry())
+pads_bottom.append(pad)
+sample.add(pad)
 
 p1 = pads_left[0]
 p2 = pads_right[0]
 
+################################
 
 Couplers=[elements.pp_transmon.PP_Transmon_Coupler(0,0,16,'left',coupler_type = 'coupler',heightl = 0.3,
                                                    w=resonator_core,s=resonator_gap,g=resonator_ground)
@@ -118,21 +121,19 @@ a2    = 0.30 # Junction width in um
 
 #jj_pp = { 'a1':a1,"a2":a2,'angle_JJ':np.pi/2}
 jj_pp = { 'a1':a1,"a2":a2,'angle_JJ':0,'manhatten':True,'h_w':5 ,'h_d':8,'squid':False }# hole sizes for the JJs
+
+
+
+
+###### define parameters for the tunable qubits
 jj_pp_flux = { 'a1':a1,"a2":a2,'angle_JJ':0,'manhatten':True,'h_w':5 ,'h_d':8,'squid':True }# hole sizes for the JJs
-
-l, t_m, t_r, gp, l_arm, h_arm, s_gap = 100, resonator_core, 6, 5, 20, 50, resonator_gap
-fluxline = {'l':l,'t_m':t_m,'t_r':t_r,'gap':gp,'l_arm':l_arm,'h_arm':h_arm,'s_gap':s_gap,'g':resonator_ground,'w':resonator_core,'s':resonator_gap}
-lph,lg,lpw,lw = 85,0,55,0.5*200/7
-rph,rg,rpw,rw = lph,lg,lpw,lw
-
-arms = {'l.ph':lph,'l.g':lg,'l.pw':lpw,'l.w':lw,'r.ph':rph,'r.g':rg,'r.pw':rpw,'r.w':rw}
-#
 l, t_m, t_r, gp, l_arm, h_arm, s_gap = 100, resonator_core, 3, 5, 20, 50, resonator_gap
 flux_distance = 20
 flux = {'l':l,'t_m':t_m,'t_r':t_r,'flux_distance':flux_distance,'gap':gp,'l_arm':l_arm,'h_arm':h_arm,'s_gap':s_gap,'g':resonator_ground,'w':resonator_core,'s':resonator_gap}
-#flux = {}
-center=(2000,1000)
-transmon1 = elements.pp_transmon.PP_Transmon(name='Q1', center=center,
+
+# draw 2 tunable qubits
+center=(4000,4500)
+transmon1_left_flux = elements.pp_transmon.PP_Transmon(name='Q1_flux_left', center=center,
                                            width=width,
                                            height=height,
                                            bridge_gap=JJ_pad_offset_x,
@@ -147,12 +148,36 @@ transmon1 = elements.pp_transmon.PP_Transmon(name='Q1', center=center,
                                            calculate_capacitance=False,
                                            remove_ground={'left': 0, 'right': 0, 'top': 0, 'bottom': 0},
                                            shoes=[],
-                                           transformations={'rotate':[-np.pi/2,center]},
+                                           transformations={'rotate':[np.pi/2,center]},
                                            fluxline_params=flux
                                            )
-center = (2000,3000)
+center=(8000,4500)
+transmon2_right_flux = elements.pp_transmon.PP_Transmon(name='Q2_flux_right', center=center,
+                                           width=width,
+                                           height=height,
+                                           bridge_gap=JJ_pad_offset_x,
+                                           bridge_w=JJ_pad_offset_y,
+                                           gap=gap,
+                                           ground_w=ground_w,
+                                           ground_h=ground_h,
+                                           ground_t=ground_t,
+                                           jj_params=jj_pp_flux,
+                                           layer_configuration=sample.layer_configuration,
+                                           Couplers=Couplers,
+                                           calculate_capacitance=False,
+                                           remove_ground={'left': 0, 'right': 0, 'top': 0, 'bottom': 0},
+                                           shoes=[],
+                                           transformations={'rotate':[np.pi/2,center]},
+                                           fluxline_params=flux
+                                           )
 
-transmon2 = elements.pp_transmon.PP_Transmon(name='PP_Transmon2',center=center,
+sample.add(transmon1_left_flux)
+sample.add(transmon2_right_flux)
+
+### add fixed frequency transmon qubit without microwave line
+center = (2000,1500)
+
+transmon1_left_fixed = elements.pp_transmon.PP_Transmon(name='Q1_fixed',center=center,
                           width = width,
                           height = height,
                           bridge_gap = JJ_pad_offset_x,
@@ -165,10 +190,9 @@ transmon2 = elements.pp_transmon.PP_Transmon(name='PP_Transmon2',center=center,
                           layer_configuration = sample.layer_configuration,
                           Couplers = Couplers,
                           calculate_capacitance = False,
-                          transformations = {'rotate':[np.pi/2,center]}
+                          transformations = {'rotate':[-np.pi/2,center]}
                           )
-sample.add(transmon2)
-sample.add(transmon1)
+sample.add(transmon1_left_fixed)
 
 sample.draw_design()
 #sample.watch()
