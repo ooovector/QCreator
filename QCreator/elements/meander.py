5d99da12111689b9968d67e99ff463e29f76e0d1
niobiumpro <kriysik@yandex.ru>
2021-10-11 12:15:29 +0300

    another way of meander creation was added

--- QCreator/elements/meander.py
+++ QCreator/elements/meander.py
@@ -4,6 +4,7 @@ from .cpw import CPW
 from copy import deepcopy
 from .core import LayerConfiguration
 
+from scipy import optimize as opt
 
 def meander_creation(name: str, initial_position: Tuple[(float, float)], w: float, s: float, g: float,
                      orientation: float,
@@ -246,3 +247,156 @@ def meander_creation(name: str, initial_position: Tuple[(float, float)], w: floa
 #         # delta {delta}
 #         # check length {check_length}
 #         # """)
+
+
+def meander_creation2(name: str, begin_point: Tuple[(float, float)],
+                    begin_orientation: float, end_point: Tuple[(float, float)], end_orientation: float,
+                    meander_length: float, length_left : float, length_right : float,
+                    radius_limit : Tuple[(float, float)], w: float, s: float, g: float,
+                    layer_configuration: LayerConfiguration):
+    """
+     :param length_left: width meander limit from begin point to the left
+     :param length_right: width meander limit from begin point to the right
+     NB! in case of orientations that are multiples of pi in half
+     these limits are determined after turning the meander by pi/2 clockwise
+     """
+    ## Find segments length with the model L = A+B+K(n-1)+n*pi*r, A - begin segment, B - end segment
+    delta_y = end_point[1] - begin_point[1]
+    delta_x = end_point[0] - begin_point[0]
+    rotation = False
+    if (begin_orientation%np.pi)!=(end_orientation%np.pi):
+        raise ValueError('begin end end orientations are incompatible')
+    ## Turn meander pi/2 clockwise if its orientations are not 0, pi, etc
+    if np.abs(np.cos(begin_orientation))!=1:
+        rotation = True
+        begin_orientation+=-np.pi/2
+        end_orientation +=-np.pi / 2
+        delta_x = end_point[1] - begin_point[1]
+        delta_y = -end_point[0] + begin_point[0]
+        begin_point = begin_point[::-1]
+        end_point = end_point[::-1]
+
+    n_max = np.int(np.abs(delta_y)/2/radius_limit[0])
+    n_min = np.int(np.abs(delta_y)/2/radius_limit[1])
+    result = []
+    for n in np.linspace(n_min, n_max, n_max-n_min+1):
+        if np.cos(begin_orientation) == np.cos(end_orientation) and n % 2 != 0:
+            pass
+        elif np.cos(begin_orientation) != np.cos(end_orientation) and n % 2 == 0:
+            pass
+        else:
+            continue
+        r = np.abs(delta_y)/2/n
+        A_max = length_right*(1+np.cos(begin_orientation))/2 \
+                + length_left*(1-np.cos(begin_orientation))/2 - (r+w/2+s)
+        B_max = (length_right-delta_x)*(1+np.cos(end_orientation))/2\
+                + (length_left+delta_x)*(1-np.cos(end_orientation))/2 - (r+w/2+s)
+
+        print(A_max, B_max)
+
+        if np.cos(begin_orientation) != np.cos(end_orientation):
+            if n!= 0:
+                K = (meander_length - delta_x*np.cos(begin_orientation))/n - np.pi*r
+                A = (K + delta_x*np.cos(begin_orientation)) / 2
+                B = A
+                print(K + delta_x*np.cos(begin_orientation))
+                if (A >= A_max) or (B >= B_max):
+                    AB = opt.lsq_linear([1,1], K + delta_x*np.cos(begin_orientation),
+                                         bounds= ([0,0], [A_max, B_max]))
+                    A = AB.x[0]
+                    B = AB.x[1]
+
+            if n == 0:
+                A = meander_length
+                B = 0
+                K = 0
+            print(A, K, B)
+
+        if np.cos(begin_orientation) == np.cos(end_orientation):
+            if n!= 1:
+                K = (meander_length - n*np.pi*r)/(n + 1) + np.abs(delta_x*np.cos(begin_orientation))/(n + 1)
+                if delta_x*np.cos(begin_orientation) > 0:  # A>B
+                    A = K
+                    B = A - delta_x*np.cos(begin_orientation)
+                else:
+                    B = K
+                    A = B + delta_x*np.cos(begin_orientation)
+            else:
+                A = (meander_length - np.pi * r) / 2
+                B = A
+                K = 0
+                if (A >= A_max) or (B >= B_max):
+                    AB = opt.lsq_linear([1,1], meander_length - np.pi * r,
+                                         bounds= ([0,0], [A_max, B_max]))
+                    A = AB.x[0]
+                    B = AB.x[1]
+
+        if (0<=A <= A_max) and (0<=B <= B_max):
+            result.append([A, K, B, n, r])
+
+    if result==[]:
+        raise ValueError('No variants were found')
+
+    if rotation:
+        begin_orientation+=np.pi/2
+        end_orientation+=np.pi/2
+        begin_point = begin_point[::-1]
+        end_point = end_point[::-1]
+        delta_y = end_point[1] - begin_point[1]
+        delta_x = end_point[0] - begin_point[0]
+        # rotated_points = []
+        # for point in points:
+        #     point = point[::-1]
+        #     rotated_points.append(point)
+        # points = rotated_points
+
+    ##Transform lengths in points for CPW
+
+    A, K, B, n, r = result[0]
+    points = []
+    shift = r
+    points.append(begin_point)
+    if n!= 0:
+        # points.append((begin_point[0] + (A + shift) * np.cos(begin_orientation), begin_point[1]))
+        # mid_segm_x = begin_point[0] + (A + shift) * np.cos(begin_orientation)
+        points.append((begin_point[0] + (A + shift) * np.cos(begin_orientation), begin_point[1] +
+                       (A + shift) * np.sin(begin_orientation)))
+        mid_segm_x = begin_point[0] + (A + shift) * np.cos(begin_orientation)
+        mid_segm_y = begin_point[1] + (A + shift) * np.sin(begin_orientation)
+
+    if n > 1:
+        for turn in range(1, np.int(n)):
+            if turn % 2 != 0:
+                # points.append((mid_segm_x, begin_point[1] + np.sign(delta_y)*2*r*turn))
+                # points.append((mid_segm_x + (K + 2*shift) * np.cos(np.pi - begin_orientation),
+                #                begin_point[1] + np.sign(delta_y) * 2 * r * turn))
+                points.append((mid_segm_x + np.sign(delta_x)*2*r*turn*np.abs(np.sin(begin_orientation)),
+                               mid_segm_y + np.sign(delta_y)*2*r*turn*np.abs(np.cos(begin_orientation))))
+                points.append((mid_segm_x + (K + 2*shift) * np.cos(np.pi - begin_orientation) +
+                               np.sign(delta_x)*2*r*turn*np.abs(np.sin(begin_orientation)),
+                               mid_segm_y + (K + 2*shift) * np.sin(np.pi + begin_orientation) +
+                               np.sign(delta_y)*2*r*turn*np.abs(np.cos(begin_orientation))))
+            else:
+                # points.append((mid_segm_x + (K + 2*shift) * np.cos(np.pi - begin_orientation),
+                #                begin_point[1] + np.sign(delta_y) * 2 * r * turn))
+                # points.append((mid_segm_x, begin_point[1] + np.sign(delta_y) * 2 * r * turn))
+                points.append((mid_segm_x + (K + 2*shift) * np.cos(np.pi - begin_orientation) +
+                               np.sign(delta_x)*2*r*turn*np.abs(np.sin(begin_orientation)),
+                               mid_segm_y + (K + 2*shift) * np.sin(np.pi + begin_orientation) +
+                               np.sign(delta_y)*2*r*turn*np.abs(np.cos(begin_orientation))))
+                points.append((mid_segm_x + np.sign(delta_x)*2*r*turn*np.abs(np.sin(begin_orientation)),
+                               mid_segm_y + np.sign(delta_y)*2*r*turn*np.abs(np.cos(begin_orientation))))
+
+    last_segm_x = points[-1][0] + np.sign(delta_x)*2*r*np.abs(np.sin(begin_orientation))
+    last_segm_y = points[-1][1] + np.sign(delta_y)*2*r*np.abs(np.cos(begin_orientation))
+    if n!= 0:
+        points.append((last_segm_x, last_segm_y))
+    points.append((end_point[0], end_point[1]))
+
+    print(points, begin_orientation, end_orientation)
+
+    meander = CPW(name, points, w, s, g, layer_configuration, r, corner_type='round',
+                  orientation1=begin_orientation, orientation2=end_orientation)
+
+
+    return result, points, meander
